5dc51db734e1f6140cf944c142db7928849c67b7
khayro <dzvindouza@gmail.com>
2024-04-30 15:43:26 +0100

    icons

--- functions.js
+++ functions.js
@@ -623,19 +623,19 @@ function generateMenuOfButtons() {
     var menu = `
     <div class="logo"></div>
         <hr class="hr-of-menu">
-        <div class="menu-item" id="rooms" onclick="goToPageOfRooms()"><div class="icon-of-button"><i class="fa-solid fa-door-closed"></i></div><div class="text-of-button">Rooms</div></div>
-        <div class="menu-item" id="clock" onclick="getPageOfClock()"><div class="icon-of-button"><i class="fa-solid fa-calendar-days"></i></div><div class="text-of-button">Clock</div></div>
+        <div class="menu-item" id="rooms" onclick="goToPageOfRooms()"><div class="icon-of-button"><i class="fa-solid fa-door-open"></i></div><div class="text-of-button">Rooms</div></div>
+        <div class="menu-item" id="clock" onclick="getPageOfClock()"><div class="icon-of-button"><i class="fa-solid fa-magnifying-glass"></i></i></div><div class="text-of-button">Search</div></div>
         <div class="menu-item" id="settings" onclick="generatePageOfSettings()"><div class="icon-of-button"><i class="fa-solid fa-gear"></i></div><div class="text-of-button">Settings</div></div>
            
         `;
 
     if (currentUser === 'admin') {
         menu += `
-        <div class="menu-item" id="historique" onclick="getPageOfArchives()"><div class="icon-of-button"><i class="fa-solid fa-box-archive"></i></div><div class="text-of-button">Archive</div></div>`;
+        <div class="menu-item" id="historique" onclick="getPageOfArchives()"><div class="icon-of-button"><i class="fa-solid fa-store"></i></div><div class="text-of-button">Archive</div></div>`;
     }
 
     menu += `
-    <div class="menu-item" id="payment" onclick=""><div class="icon-of-button"><i class="fa-solid fa-cart-shopping"></i></div><div class="text-of-button">Payment</div></div>
+    <div class="menu-item" id="payment" onclick=""><div class="icon-of-button"><i class="fa-regular fa-credit-card"></i></div><div class="text-of-button">Payment</div></div>
     <hr class="hr-of-menu">
     <div class="menu-item" id="log-out" onclick="logOut()"><div class="icon-of-button"><i class="fa-solid fa-right-from-bracket"></i></div><div class="text-of-button">Log out</div></div>
     <div id="div-of-acc">
@@ -662,12 +662,24 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function makeActive(element) {
+    // Remove 'active' class from previously active menu item and its icon
     const currentlyActive = document.querySelector('.menu-item.active');
     if (currentlyActive) {
         currentlyActive.classList.remove('active');
+        const activeIcon = currentlyActive.querySelector('.icon-of-button i');
+        if (activeIcon) {
+            activeIcon.classList.remove('active-icon');  // assuming 'active-icon' is the class for active state icons
+        }
     }
+
+    // Add 'active' class to the current element and its icon
     element.classList.add('active');
+    const icon = element.querySelector('.icon-of-button i');
+    if (icon) {
+        icon.classList.add('active-icon');  // add this class to the icon as well
+    }
 }
+
 
 function handleMenuAction(id) {
     switch (id) {
